ff020f0d9ffc71dd7a8a2bf97b6a3eaedb89edf1
KristofferLN <kristoffer.LN@hotmail.com>
2026-02-04 20:13:06 +0100

    hurtigt tjek med wwwwroot config. ellers tilbage igen

--- _testprojektet/settings.py
+++ _testprojektet/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = '_testprojektet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": "/home/site/wwwroot/db.sqlite3",
     }
 }
+# 'NAME': BASE_DIR / 'db.sqlite3',
 
 
 # Password validation
