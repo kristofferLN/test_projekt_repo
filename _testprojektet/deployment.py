7513543165afcb0b1e85c4063d48b0c3d6861e90
KristofferLN <kristoffer.LN@hotmail.com>
2026-02-04 17:31:12 +0100

    har slettet postgresql kode. måske kan man bare køre sqllite i prod

--- _testprojektet/deployment.py
+++ _testprojektet/deployment.py
@@ -26,10 +26,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-DATABASES = { 
-    'default': { 
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'testprojekt-server' 
-         } }
 
 #azure giver også disse variabler, via en environmentalvariable. vi laver referendce dertil, fordi det er nødvendigt at specificere, men også fordi vi ikke kan specificiere sensitive detaljer her.
